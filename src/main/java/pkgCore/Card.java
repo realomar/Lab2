549a4edbb34dfaf36d1a8e0e21793be0ed3bc33c
Omar Ahmad <eatmorerice@outlook.com>
2018-02-21 08:27:05 -0500

    Initial commit

---
+++ src/main/java/pkgCore/Card.java
@@ -0,0 +1,22 @@
+package pkgCore;
+import pkgEnum.*;
+public class Card implements Comparable {
+
+	//	TODO: Card has two attributes, eRank and eSuit, add these attributes
+	
+	
+	//	TODO: Build a constructor for Card passing in eRank and eSuit
+	
+	
+	//	TODO: Add a public 'getter' method for eRank and eSuit.  
+	//			Add a private 'setter' method for eRank and eSuit
+	
+	@Override
+	public int compareTo(Object o) {
+		Card c = (Card) o;
+		return 0;
+		//return c.geteRank().compareTo(this.geteRank());
+
+}
+	
+}
