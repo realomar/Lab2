d3f2760879ec2cb4916920ce35ff1e155692e693
Omar Ahmad <eatmorerice@outlook.com>
2018-02-28 08:17:16 -0500

    Added getters and setters in Card

--- src/main/java/pkgCore/Card.java
+++ src/main/java/pkgCore/Card.java
@@ -2,7 +2,7 @@ package pkgCore;
 import pkgEnum.*;
 public class Card implements Comparable {
 
-	//  TODO: Add eRank and eSuit as attributes.
+	//  Add eRank and eSuit as attributes.
 	private eRank eRank;
 	private eSuit eSuit;
 	
@@ -13,10 +13,7 @@ public class Card implements Comparable {
 		this.eSuit = eSuit;
 	}
 	
-	
-	//	TODO: Add a public 'getter' method for eRank and eSuit.  
-	//			Add a private 'setter' method for eRank and eSuit
-	
+
 	@Override
 	public int compareTo(Object o) {
 		Card c = (Card) o;
@@ -24,5 +21,25 @@ public class Card implements Comparable {
 		//return c.geteRank().compareTo(this.geteRank());
 
 }
+
+
+	public eRank geteRank() {
+		return eRank;
+	}
+
+
+	private void seteRank(eRank eRank) {
+		this.eRank = eRank;
+	}
+
+
+	public eSuit geteSuit() {
+		return eSuit;
+	}
+
+
+	private void seteSuit(eSuit eSuit) {
+		this.eSuit = eSuit;
+	}
 	
 }
