97e937f94aa6630e6125f545665a3342c29545ee
Omar Ahmad <eatmorerice@outlook.com>
2018-02-28 08:47:39 -0500

    Partially added scoring in Hand

--- src/main/java/pkgCore/Hand.java
+++ src/main/java/pkgCore/Hand.java
@@ -13,26 +13,38 @@ public class Hand {
 		
 	}
 	
-	public int[] ScoreHand()
-	{
+	public int[] ScoreHand(){
+		
 		int [] iScore = new int[2];
 		
-		iScore[0] = 5;
-		iScore[1] = 10;
+		iScore[0] = 0;
+		iScore[1] = 0;
 		
 		Collections.sort(cards);
 		
 		
-		for (Card c: cards)
-		{
+		for (Card c: cards){
+			if(c.geteRank().getiRankNbr()<11){
+				iScore[0] += c.geteRank().getiRankNbr();
+				iScore[1] += c.geteRank().getiRankNbr();
+			}
+			else if(c.geteRank().getiRankNbr()<14) {
+				iScore[0] += 10;
+				iScore[1] += 10;
+			}
+			else {
+				iScore[0] += 1;
+				iScore[1] += 11;
+			}
 			//	TODO: Determine the score.  
 			//			Cards:
-			//			2-3-4 - score = 11
+			//			2-3-4 - score = 9
 			//			5-J-Q - score = 25
 			//			5-6-7-2 - score = 20
 			//			J-Q	- score = 20
 			//			8-A = score = 9 or 19
 			//			4-A = score = 5 or 15
+			
 		}
 		
 		return iScore;
